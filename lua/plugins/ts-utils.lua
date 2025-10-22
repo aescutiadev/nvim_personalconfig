f10bec7902c35930c8b5566c303b22019b923f45
aescutiadev <aescutiadev@gmail.com>
2025-10-21 19:43:30 -0600

    feat: colorizer for tailwind

--- lua/plugins/ts-utils.lua
+++ lua/plugins/ts-utils.lua
@@ -47,4 +47,14 @@ return {
       },
     },
   },
+  {
+    "NvChad/nvim-colorizer.lua",
+    optional = true,
+    opts = {
+      user_default_options = {
+        names = true,
+        tailwind = true,
+      },
+    },
+  },
 }
